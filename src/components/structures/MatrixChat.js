8e85c703b18427241557ef080508f45c44496559
Damir Jelić <poljar@termina.org.uk>
2019-10-01 16:32:46 +0200

    MatrixChat: Add the profile only if one exists for historic events.

--- src/components/structures/MatrixChat.js
+++ src/components/structures/MatrixChat.js
@@ -2188,9 +2188,11 @@ export default React.createClass({
 
                 // TODO do we need to check if the event has all the valid
                 // attributes?
+                let profile = {}
+                if (e.sender in profiles) profile = profiles[e.sender];
                 const object = {
                     event: e,
-                    profile: profiles[e.sender]
+                    profile: profile
                 };
                 return object;
             });

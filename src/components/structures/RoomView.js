be233efe9c49f19cf5b930aa0d075091c93441f5
Damir Jelić <poljar@termina.org.uk>
2019-10-03 14:05:02 +0200

    RoomView: Style fixes.

--- src/components/structures/RoomView.js
+++ src/components/structures/RoomView.js
@@ -1129,7 +1129,8 @@ module.exports = React.createClass({
             const combinedSearchFunc = async (searchTerm) => {
                 // Create two promises, one for the local search, one for the
                 // server-side search.
-                const serverSidePromise = MatrixClientPeg.get().searchRoomEvents({
+                const client = MatrixClientPeg.get();
+                const serverSidePromise = client.searchRoomEvents({
                     term: searchTerm,
                 });
                 const localPromise = localSearchFunc(searchTerm);
@@ -1159,13 +1160,15 @@ module.exports = React.createClass({
                 };
 
                 result.count = localResult.count + serverSideResult.count;
-                result.results = localResult.results.concat(serverSideResult.results).sort(compare);
-                result.highlights = localResult.highlights.concat(serverSideResult.highlights);
+                result.results = localResult.results.concat(
+                    serverSideResult.results).sort(compare);
+                result.highlights = localResult.highlights.concat(
+                    serverSideResult.highlights);
 
                 return result;
             };
 
-            const localSearchFunc = async function(searchTerm, roomId = undefined) {
+            const localSearchFunc = async (searchTerm, roomId = undefined) => {
                 const searchArgs = {
                     search_term: searchTerm,
                     before_limit: 1,
@@ -1174,10 +1177,11 @@ module.exports = React.createClass({
                 };
 
                 if (roomId !== undefined) {
-                    searchArgs.roomId = roomId;
+                    searchArgs.room_id = roomId;
                 }
 
-                const localResult = await platform.searchEventIndex(searchArgs);
+                const localResult = await platform.searchEventIndex(
+                    searchArgs);
 
                 const response = {
                     search_categories: {

a3dc8f6394c23bde97194f447b18de76565919a7
waylon531 <waylon.cude@finzdani.net>
2020-03-29 12:08:32 -0700

    Add marquee tag
    Make the internet fun again

--- src/HtmlUtils.js
+++ src/HtmlUtils.js
@@ -227,6 +227,7 @@ const sanitizeHtmlParams = {
         'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'blockquote', 'p', 'a', 'ul', 'ol', 'sup', 'sub',
         'nl', 'li', 'b', 'i', 'u', 'strong', 'em', 'strike', 'code', 'hr', 'br', 'div',
         'table', 'thead', 'caption', 'tbody', 'tr', 'th', 'td', 'pre', 'span', 'img',
+        'marquee',
     ],
     allowedAttributes: {
         // custom ones first:

75918161d67394c8d7558da2c56f9bc0ce6bcc42
Waylon Cude <waylon.cude@finzdani.net>
2020-10-11 17:12:34 -0700

    Add youtube video previews
    Hopefully this works lol

--- src/components/views/rooms/LinkPreviewWidget.tsx
+++ src/components/views/rooms/LinkPreviewWidget.tsx
@@ -110,6 +110,65 @@ export default class LinkPreviewWidget extends React.Component<IProps> {
         );
         const needsTooltip = PlatformPeg.get()?.needsUrlTooltips() && this.props.link !== title;
 
+        let maybeEmbed: string = ""
+        let id: string = ""
+
+
+        if (this.props.link.startsWith("https://youtu.be/"))  {
+            //Remove beginning of url to get video id
+            const tail = this.props.link.slice(17);
+            var part = tail.split("?");
+            if (part.length == 2) {
+                var split=part[1].split("&");
+                var params: {[key: string] : string} = {};
+                for (let i=0; i<split.length; i++) {
+                    const t=split[i];
+                    const result=t.split("=");
+                    // Can javascript segfault?
+                    params[result[0]] = result[1];
+                }
+                // Add in the start time if it exists
+                if (params["t"] !== null) {
+                    id = part[0] + "?start=" + params["t"];
+                } else {
+                    id = part[0];
+                }
+            } else {
+                // In this case there's no extra GET parameters
+                id = tail;
+            }
+        } else if (this.props.link.startsWith("https://www.youtube.com/watch")
+                || this.props.link.startsWith("https://youtube.com/watch")) {
+            //Remove beginning of url to get GET parameters
+            var chunk;
+            if (this.props.link.startsWith("https://www.youtube.com/watch")) {
+                chunk=this.props.link.slice(30);
+            } else {
+                chunk=this.props.link.slice(26);
+            }
+            var params: {[key: string] : string} = {};
+            const split=chunk.split("&");
+            for (let i=0; i<split.length; i++) {
+                const t=split[i];
+                const result=t.split("=");
+                // Can javascript segfault?
+                params[result[0]] = result[1];
+            }
+            // Add in the start time if it exists
+            if (params["t"] !== null) {
+                id = params["v"] + "?start=" + params["t"];
+            } else {
+                id = params["v"];
+            }
+        }
+
+        if (id != "") {
+            const url="https://www.youtube.com/embed/" + id
+            maybeEmbed = `<iframe width="560" height="315" src=${url} frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; fullscreen" allowfullscreen></iframe>`
+
+        }
+
+
         return (
             <div className="mx_LinkPreviewWidget">
                 <div className="mx_LinkPreviewWidget_wrapImageCaption">
@@ -130,6 +189,7 @@ export default class LinkPreviewWidget extends React.Component<IProps> {
                         <div className="mx_LinkPreviewWidget_description">
                             <Linkify>{description}</Linkify>
                         </div>
+                        {maybeEmbed}
                     </div>
                 </div>
                 {this.props.children}
